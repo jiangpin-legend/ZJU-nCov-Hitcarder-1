6b8cf94d82f84498809afc416fd84e870eaefb29
jiangpin-legend <60228505+jiangpin-legend@users.noreply.github.com>
2022-10-03 20:47:08 +0800

    random start time

--- hitcarder.py
+++ hitcarder.py
@@ -222,6 +222,11 @@ if __name__ == "__main__":
     username = os.environ['USERNAME']
     password = os.environ['PASSWORD']
 
+    sleep_time = randomint(10,50)
+    print("Random sleep"+str(sleep_time)+"min")
+    time.sleep(sleep_time*60)
+    print("Sleep Finished")
+    
     ret, msg = main(username, password)
     print(ret, msg)
     if ret == 1:
@@ -229,10 +234,7 @@ if __name__ == "__main__":
         ret, msg = main(username, password)
         print(ret, msg)
     
-    sleep_time = randomint(10,50)
-    print("Random sleep"+str(sleep_time)+"min")
-    time.sleep(sleep_time*60)
-    print("Sleep Finished")
+   
     
     dingtalk_token = os.environ.get('DINGTALK_TOKEN')
     if dingtalk_token:
